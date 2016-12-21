633b8a7d77c6b63ad335a0df600734cd97173ae2
Ann Caplin <ann.caplin@gmail.com>
2016-12-21 17:20:04 -0500

    Temperature converter GUI seems to work

--- TempGUI/TempWindow.java
+++ TempGUI/TempWindow.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.event.*;
-public class TempWindow extends JFrame{
+import java.awt.event.*;
+public class TempWindow extends JFrame implements ActionListener{
  private Container pane;
  private JButton toF, toC;
  private JLabel ans;
@@ -13,7 +13,38 @@ public class TempWindow extends JFrame{
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	pane = this.getContentPane();
 	pane.setLayout(new FlowLayout());
-	b = new JButton("C to F");
+	toF = new JButton("C to F");
+	toF.setActionCommand("CF");
+	toF.addActionListener(this);
+	toC = new JButton("F to C");
+	toC.setActionCommand("FC");
+	toC.addActionListener(this);
+	ans = new JLabel();
+	temp = new JTextField(12);
+	pane.add(temp);
+	pane.add(toF);
+	pane.add(toC);
+	pane.add(ans);
+    }
+    public void actionPerformed(ActionEvent e){
+	try{
+	switch(e.getActionCommand()){
+	case "CF":
+	    ans.setText(""+TempConvert.CtoF(Double.parseDouble(temp.getText())));
+	    break;
+	case "FC":
+	    ans.setText(""+TempConvert.FtoC(Double.parseDouble(temp.getText())));
+	    break;
+	}
+	}
+	catch(NumberFormatException b){
+	    ans.setText("Invalid input");
+	}
+    }
+    
+    public static void main(String[] args){
+	TempWindow w = new TempWindow();
+	w.setVisible(true);
     }
 
 }
